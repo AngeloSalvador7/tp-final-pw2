12d240699e7cbba0100a4cba83db30850ce01b85
AngeloSalvador7 <aordonezgarcia@alumno.unlam.edu.ar>
2021-06-28 13:55:05 -0300

    ABM - Vehiculo. Vistas, parciales, controladores y modelo de VEHICULO.

---
+++ controller/SupervisorVehiculosController.php
@@ -0,0 +1,87 @@
+<?php
+class SupervisorVehiculosController extends SessionCheck
+{
+    private $render;
+    private $vehiculosModel;
+
+    public function __construct($render, $vehiculosModel)
+    {
+        parent::__construct("SUPERVISOR");
+        $this->render = $render;
+        $this->vehiculosModel = $vehiculosModel;
+    }
+
+    public function execute()
+    {
+        $this->VehiculosView();
+    }
+
+    private function VehiculosView()
+    {
+        if(!$_SESSION['usuario']){
+            header("Location: /");
+            exit();
+        }
+
+        $data['usuario']=$_SESSION['usuario'];
+        $data['vistaVehiculos']=true;
+        $data['vehiculos']=$this->vehiculosModel->getVehiculos();
+
+        if (!$data['vehiculos']){
+            $data['mensaje'] = "No hay vehiculos registrados";
+        }
+
+        echo $this->render->render("view/vehiculoView.php", $data);
+    }
+
+    public function agregarVehiculo()
+    {
+        $data['usuario']=$_SESSION['usuario'];
+        $data['vistaAgregarVehiculo']=true;
+        $data['vehiculo']=$this->vehiculosModel->getVehiculos();
+
+        echo $this->render->render("view/vehiculoView.php", $data);
+    }
+
+    public function insertarVehiculo()
+    {
+        $data['usuario']=$_SESSION['usuario'];
+        $data['vistaVehiculos']=true;
+        $this->vehiculosModel->agregarNuevoVehiculo($_POST);
+        header('location: http://localhost/vehiculos');
+        exit();
+    }
+
+    public function editarVehiculo()
+    {
+        $data['usuario']=$_SESSION['usuario'];
+        $data['vistaModificarVehiculo']=true;
+        $data['vehiculos']=$this->vehiculosModel->getVehiculos();
+        $data['arrastre']=$this->vehiculosModel->getArrastres();;
+        if (!$data['vehiculos']){
+            $data['mensaje'] = "No hay vehiculos registrados";
+        }
+        echo $this->render->render("view/vehiculoView.php", $data);
+    }
+
+    public function borrarVehiculo()
+    {
+        $this->vehiculosModel->dropVehiculo($_POST['borrar_id']);
+        $this->editarVehiculo();
+    }
+
+    public function actualizarVehiculo()
+    {
+        $this->vehiculosModel->actualizarVehiculo($_POST);
+        $this->editarVehiculo();
+    }
+
+    public function modificarVehiculo()
+    {
+        $data['vistaModificacionDeVehiculo']=true;
+        $data['arrastre']=$this->vehiculosModel->getArrastres();;
+        $data['vehiculo']=$this->vehiculosModel->getUnicoVehiculo($_POST['modificar_id']);
+
+        echo $this->render->render("view/vehiculoView.php", $data);
+    }
+}
