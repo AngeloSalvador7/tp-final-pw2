aa09f22ce192ed83cc4d4c1148d5c4052e3df9c9
zaito18 <k4tnet@outlook.com>
2021-06-28 22:50:48 -0300

    abm clientesv4

--- sql.sql
+++ sql.sql
@@ -8,9 +8,6 @@ VALUES (33023745, "1987-07-15", "Pedro", "Alonso", "supervisor@gmail.com", "1234
 INSERT INTO empleado(dni, fecha_nacimiento, nombre, apellido, email, clave, id_rol) 
 VALUES (346023745, "1990-11-05", "Juana", "Alfonso", "alfonsoa@gmail.com", "juanita22", 3);
 
-INSERT INTO empleado(dni, fecha_nacimiento, nombre, apellido, email, clave, id_rol) 
-VALUES (3798945, "1988-10-25", "Franco", "Davila", "davilaf@gmail.com", "franquito", null);
-
 Insert  into tipo_arrastre(descripcion) values ('tanque'),('acoplado'),('cisterna');
 
 INSERT INTO `transporte`.`cliente` (`id`, `denominacion`, `razon_social`, `cuit`, `direccion`, `telefono`, `email`) 
