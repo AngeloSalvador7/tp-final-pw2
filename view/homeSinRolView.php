d357b6865e67112719e792309ea67cd9b17d72de
marcelo1995tate <mzelaya@alumno.unlam.edu.ar>
2021-07-18 20:51:02 -0300

    refactor1

--- view/homeSinRolView.php
+++ view/homeSinRolView.php
@@ -5,21 +5,12 @@
     <div class="container-fluid">
         <ul class="navbar-nav navbar-expand-lg">
             <li class="nav-item active px-3">
-                <a class="nav-link" href="home">{{nombre}}</a>
-            </li>
-            <li class="nav-item px-3">
-                <a class="nav-link" href="home">Viajes</a>
-            </li>
-            <li class="nav-item px-3">
-                <a class="nav-link" href="home">Empleados</a>
-            </li>
-            <li class="nav-item px-3">
-                <a class="nav-link" href="home">Estadisticas</a>
+                <a class="nav-link" href="home">Home</a>
             </li>
         </ul>
     </div>
 </nav>
 
-<h1>Esperando asignacion de rol</h1>
+<h1 class="text-center my-5">Esperando asignacion de rol</h1>
 
 {{> footer}}
